b304df394d28def3b723f034de79b1efb5efbf4e
mahipal <mahimalviya3215@gmail.com>
2020-08-21 01:02:02 +0530

    Initial commit

---
+++ app/src/main/java/com/mahipal/calculator/CalculatorStorage.kt
@@ -0,0 +1,6 @@
+package com.mahipal.calculator
+
+/**
+ * Created by Mahipal on 20/8/20
+ */
+data class CalculatorStorage(val inputString:String,val outputResult:String)
---
+++ app/src/main/java/com/mahipal/calculator/HistoryAdapter.kt
@@ -0,0 +1,38 @@
+package com.mahipal.calculator
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.layout_history_item.view.*
+
+/**
+ * Created by Mahipal on 21/8/20
+ */
+class HistoryAdapter(private val list:List<CalculatorStorage>): RecyclerView.Adapter<HistoryAdapter.HistoryHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryHolder {
+        return HistoryHolder(LayoutInflater.from(parent.context).inflate(R.layout.layout_history_item,parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: HistoryHolder, position: Int) {
+        holder.bindItem(list[position])
+    }
+
+    inner class HistoryHolder(itemView:View): RecyclerView.ViewHolder(itemView) {
+
+        fun bindItem(calculatorStorage: CalculatorStorage) {
+            itemView.tv_input.text = calculatorStorage.inputString
+
+            if (calculatorStorage.outputResult.equals("0.0",true)) {
+                itemView.tv_result.text = ""
+            } else {
+                itemView.tv_result.text = calculatorStorage.outputResult
+            }
+
+        }
+    }
+}
